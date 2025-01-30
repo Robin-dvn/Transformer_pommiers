dcdc76641b4231372972b7bb444f5d8c4a79f673
Robin Davenne <robin.davenne@grenoble-inp.org>
2025-01-30 13:55:09 +0100

    Main file for training, wandb

--- main.py
+++ main.py
@@ -1,16 +1,144 @@
 from DatasetCreator import DatasetCreator
 from pathlib import Path
+from transformer import Transformer
+from PommierDataset import PommierDataset,collate_fn
+from torch.utils.data import DataLoader,random_split
+from tqdm import tqdm
+
+import torch.optim as optim
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+import wandb
+
+def model_size_mb(model):
+    total_size = sum(p.numel() * p.element_size() for p in model.parameters() if p.requires_grad)
+    return total_size / (1024 ** 2)  # Convertit en Mo
+
 if __name__ == "__main__":
+
+    exp_name = "Simple Transformer"
+    dataset_name = "100 sample de chaque type"
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    ########## CONSTANTS ##################
+
+    BATCH_SIZE = 32
+    VAL_SPLIT = 0.8
+    IN_VOCAB_SIZE = 16
+    OUT_VOCAB_SIZE = 11
+    PADDING_IDX = 0
+    N_HEAD = 8
+    D_MODEL = 512
+    LR = 0.001
+    NB_EPOCH = 500
+
+    ########## WANDB Project ##############
+    wandb.init(
+        # set the wandb project where this run will be logged
+        project="Topologie-Pommiers",
+
+        # track hyperparameters and run metadata
+        config={
+        "learning_rate": LR,
+        "Val split": VAL_SPLIT,
+        "architecture": exp_name,
+        "dataset": dataset_name,
+        "batch size": BATCH_SIZE,
+        "Dimension model": D_MODEL,
+        "Numer of heads": N_HEAD,
+        "epochs": NB_EPOCH,
+        }
+    )
+    ########## DATASET GENERATION #########
     
     outpath = "out"
-    data_creator = DatasetCreator(outpath,1234,4,70,10000)
+    data_creator = DatasetCreator(outpath,1234,4,70,1)
     if Path(outpath+"/dataset.csv").exists():
         data_creator.load_data(outpath+"/dataset.csv")
     else:
         data_creator.create_data(True,True)
-    if Path(outpath+"/dataset_tokenised.csv").exists():
-        data_creator.load_tokenised_data(outpath+"/dataset_tokenised.csv")
-        data_creator.tokens_to_id()
-    else:
-        data_creator.tokenise_data(rewrite=True)
+
+   ######### PYTORCH DATASET CREATION ##### 
+
+    dataset = PommierDataset(outpath+ "/dataset.csv")
+    train_size = int(VAL_SPLIT*len(dataset))
+    val_size = len(dataset) - train_size
+    train_split,val_split = random_split(dataset,[train_size, val_size])
+
+    train_loader = DataLoader(train_split,batch_size=BATCH_SIZE,shuffle=True,collate_fn=collate_fn)
+    val_loader = DataLoader(val_split,batch_size=BATCH_SIZE,shuffle=True,collate_fn=collate_fn)
+
+    ######### MODEL-OPTIMIZER-LOSS#########
+
+    model = Transformer(IN_VOCAB_SIZE,OUT_VOCAB_SIZE,D_MODEL,N_HEAD,PADDING_IDX)
+    model.to(device)
+    num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    size_mb = model_size_mb(model)
+    wandb.config.update({"num_params": num_params,"size_mb":size_mb})
+    print(f"Nombre de paramètres ajouté à wandb : {num_params:,}")
+    print(f"Le modèle occupe environ {size_mb:.2f} Mo en mémoire.")
+
+    optimizer = optim.Adam(model.parameters(),LR)
+
+    criterion = nn.CrossEntropyLoss(ignore_index = PADDING_IDX) 
+
+    ############### TRAINING ##############
+
+    for epoch in range(NB_EPOCH):
+
+        ############### train split ##############
+        model.train()
+        total_train_loss = 0
+        for enc_inp,dec_inp,dec_target in tqdm(train_loader):
+
+            enc_inp.to(device)
+            dec_inp.to(device)
+            dec_target.to(device)
+            padding_mask = (dec_inp == PADDING_IDX).to(torch.float32)
+
+            logits = model(enc_inp,dec_inp,padding_mask)
+
+            logits = logits.view(-1,logits.size(-1))
+            dec_target = dec_target.view(-1)
+            loss = criterion(logits,dec_target)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            total_train_loss += loss.item()
+            wandb.log({"train_loss":loss.item()})
+
+        ############### val split ##############
+        model.eval()
+        total_eval_loss = 0
+        with torch.no_grad(): 
+
+            for enc_inp,dec_inp,dec_target in val_loader:
+
+                enc_inp = enc_inp.to(device)
+                dec_inp = dec_inp.to(device)
+                dec_target = dec_target.to(device)
+                padding_mask = (dec_inp == PADDING_IDX).to(torch.float32)
+
+                logits = model(enc_inp,dec_inp,padding_mask)
+
+                logits = logits.view(-1,logits.size(-1))
+                dec_target = dec_target.view(-1)
+                loss = criterion(logits,dec_target)
+                total_eval_loss += loss.item()
+                wandb.log({"val loss":loss.item()})
+        if epoch % 10 == 0:
+            avg_train_loss = total_train_loss / len(train_loader)
+            avg_val_loss = total_eval_loss / len(val_loader)
+            print(f"[INFO] Epoch {epoch} : train loss = {avg_train_loss:.4f}, val loss = {avg_val_loss:.4f}")
+
+
+
+
+
+
+
+
+    
         
--- transformer.py
+++ transformer.py
@@ -1,11 +1,13 @@
 
 from PommierDataset import PommierDataset,collate_fn
 from torch.utils.data import DataLoader
+from torch import Tensor
 
 import math
 import torch 
 import torch.nn as nn
 import torch.nn.functional as F
+
 
 class PositionalEncoding(nn.Module):
 
@@ -32,51 +34,54 @@ class Transformer(nn.Module):
         super(Transformer,self).__init__()
         self.embed = nn.Embedding(in_vocab_size,d_model,padding_idx=padding_idx)
         self.posEmbed = PositionalEncoding(d_model)
-        self.transformer = nn.Transformer(d_model,n_head,batch_first=True)
+        self.transformer = nn.Transformer(d_model,n_head,batch_first=True,dropout=0.)
         self.fc_l = nn.Linear(d_model,out_vocab_size)
     
-    def forward(self,src,tgt,tgt_key_padding_mask):
-
+    def forward(self, src: Tensor, tgt: Tensor, tgt_key_padding_mask: Tensor) -> Tensor:
         s_emb = self.embed(src)
         t_emb = self.embed(tgt)
         s_p_emb = self.posEmbed(s_emb)
         t_p_emb = self.posEmbed(t_emb)
-        print(tgt.shape)
-        out_trans = self.transformer(s_p_emb,t_p_emb,tgt_mask = nn.Transformer.generate_square_subsequent_mask(tgt.shape[1]), tgt_is_causal = True,tgt_key_padding_mask =tgt_key_padding_mask )
+
+        tgt_mask = nn.Transformer.generate_square_subsequent_mask(tgt.shape[1])
+        
+        out_trans = self.transformer(
+            s_p_emb, t_p_emb, tgt_mask=tgt_mask, tgt_is_causal=True, tgt_key_padding_mask=tgt_key_padding_mask
+        )
         out = self.fc_l(out_trans)
         return out
 
+if __name__ == "__main__":
+    dataset = PommierDataset("out/dataset.csv")
+    dataloader = DataLoader(dataset,3,False,collate_fn=collate_fn)
 
-dataset = PommierDataset("out/dataset.csv")
-dataloader = DataLoader(dataset,3,False,collate_fn=collate_fn)
-
-batch = next(iter(dataloader))
+    batch = next(iter(dataloader))
 
 
 
-pad_token_id = 0
-enc_inputs, dec_inputs, dec_targets = batch
-padding_mask = (dec_inputs == pad_token_id)
+    pad_token_id = 0
+    enc_inputs, dec_inputs, dec_targets = batch
+    padding_mask = (dec_inputs == pad_token_id)
 
-model = Transformer(16,11,4,2,0)
-out = model(enc_inputs,dec_inputs,padding_mask)
+    model = Transformer(16,11,4,2,0)
+    out = model(enc_inputs,dec_inputs,padding_mask)
 
-pad_token_id = 0
+    pad_token_id = 0
 
-# Transformer logits et targets
-print(out.shape)
-logits = out.view(-1, out.size(-1))  # (batch_size * seq_len, vocab_size)
-print(logits.shape)
-print(dec_targets.shape)
-targets = dec_targets.view(-1)  # (batch_size * seq_len,)
-print(targets.shape)
+    # Transformer logits et targets
+    print(out.shape)
+    logits = out.view(-1, out.size(-1))  # (batch_size * seq_len, vocab_size)
+    print(logits.shape)
+    print(dec_targets.shape)
+    targets = dec_targets.view(-1)  # (batch_size * seq_len,)
+    print(targets.shape)
 
-# Calculer la perte en ignorant le padding
-loss = F.cross_entropy(logits, targets, ignore_index=pad_token_id)
-print("Loss:\n",loss)
-print("Encoder Inputs:\n", enc_inputs)
-print("Decoder Inputs:\n", dec_inputs)
-print("Decoder Targets:\n", dec_targets)
-print("outdu transformer:\n",out)
-print("targets:\n",targets)
-print("Logits:\n",logits)
+    # Calculer la perte en ignorant le padding
+    loss = F.cross_entropy(logits, targets, ignore_index=pad_token_id)
+    print("Loss:\n",loss)
+    print("Encoder Inputs:\n", enc_inputs)
+    print("Decoder Inputs:\n", dec_inputs)
+    print("Decoder Targets:\n", dec_targets)
+    print("outdu transformer:\n",out)
+    print("targets:\n",targets)
+    print("Logits:\n",logits)
