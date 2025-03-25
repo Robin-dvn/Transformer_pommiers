57353d77cb850e921bec80a8663df16fdb6aafec
Robin Davenne <robin.davenne@grenoble-inp.org>
2025-03-25 15:53:14 +0100

    modif validation

--- main_decoder_only.py
+++ main_decoder_only.py
@@ -47,40 +47,30 @@ if __name__ == "__main__":
         print(f"=== {'Test' if test else 'Essai'} baseline ===")
         validator_baseline = train_generate_validate_pipeline(base_config,sync_wandb=True)
 
-        # # Essai cyclical learning rate
-        # config_cyclical = base_config.copy()
-        # config_cyclical["scheduler"] = {
-        #     "name": "cyclical",
-        #     "params": {"base_lr": 5e-8, "max_lr": 5e-5, "step_size_up": 782}
-        # }
-        # config_cyclical["graph_name"] = f"DO_NBL-{layers}_DM-32_DFF-{ff}_cyclical"
+        # Essai cyclical learning rate
+        config_cyclical = base_config.copy()
+        config_cyclical["scheduler"] = {
+            "name": "cyclical",
+            "params": {"base_lr": 5e-8, "max_lr": 5e-5, "step_size_up": 782}
+        }
+        config_cyclical["graph_name"] = f"DO_NBL-{layers}_DM-32_DFF-{ff}_cyclical"
 
-        # print(f"=== {'Test' if test else 'Essai'} cyclical learning rate ===")
-        # validator_cyclical = train_generate_validate_pipeline(config_cyclical)
+        print(f"=== {'Test' if test else 'Essai'} cyclical learning rate ===")
+        validator_cyclical = train_generate_validate_pipeline(config_cyclical,sync_wandb=True)
 
-        # #Essai de early stopping
-        # config_early = base_config.copy()
-        # config_early["scheduler"] = {
-        #     "name": "None",
-        #     "params": {}
-        # }
-        # config_early["early_stopping"] = {
-        #     "name": "patience",
-        #     "params": {"patience": 100, "verbose": True, "delta": 0.001}
-        # }
-        # config_early["graph_name"] = f"DO_NBL-{layers}_DM-32_DFF-{ff}_early"
-        # print(f"=== {'Test' if test else 'Essai'} early stopping ===")
-        # validator_early = train_generate_validate_pipeline(config_early)
+        #Essai de early stopping
+        config_early = base_config.copy()
+        config_early["scheduler"] = {
+            "name": "None",
+            "params": {}
+        }
+        config_early["early_stopping"] = {
+            "name": "patience",
+            "params": {"patience": 100, "verbose": True, "delta": 0.001}
+        }
+        config_early["graph_name"] = f"DO_NBL-{layers}_DM-32_DFF-{ff}_early"
+        print(f"=== {'Test' if test else 'Essai'} early stopping ===")
+        validator_early = train_generate_validate_pipeline(config_early,sync_wandb=True)
 
-        # #Essai de précision auto (précision adaptative)
-        # config_auto_precision = base_config.copy()
-        # config_auto_precision["scheduler"] = {
-        #     "name": "None",
-        #     "params": {}
-        # }
-        # config_auto_precision["early_stopping"] = {"name": "None", "params": {}}
-        # config_auto_precision["auto_precision"] = True
-        # config_auto_precision["graph_name"] = f"DO_NBL-{layers}_DM-32_DFF-{ff}_auto_precision"
-        # print("=== Essai de précision auto ===")
-        # validator_auto_precision = train_generate_validate_pipeline(config_auto_precision)
+
 
